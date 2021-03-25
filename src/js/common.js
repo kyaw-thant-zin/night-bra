ed3028ea20ab67c567a05ac69615d8c0c1f885ac
Kyaw Thant Zin <52561987+kyaw-thant-zin@users.noreply.github.com>
2021-03-25 13:21:43 +0630

    Minify CSS and JS

--- src/js/common.js
+++ src/js/common.js
@@ -5,15 +5,12 @@ var itemImgPaddingRight = 0;
 var totalSpace = 0;
 
 $(window).on('load', function () { //全ての読み込みが完了したら実行
-  console.log('load');
   $('#page').css('display', 'none');
   $('#loader').fadeIn(1000);
   $('body').addClass('no-scroll');
 });
 
 $(document).ready(function () {
-
-  console.log('ready');
 
   $('#loader-bg').delay(3000).fadeOut(400, function () {
     $('body').removeClass('no-scroll');
