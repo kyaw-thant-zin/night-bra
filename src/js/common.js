69951c8a2148998f4661414d117df054a80dd123
Kyaw Thant Zin <52561987+kyaw-thant-zin@users.noreply.github.com>
2021-03-25 13:51:57 +0630

    Update JS

--- src/js/common.js
+++ src/js/common.js
@@ -5,17 +5,17 @@ var itemImgPaddingRight = 0;
 var totalSpace = 0;
 
 $(window).on('load', function () { //全ての読み込みが完了したら実行
-  $('#page').css('display', 'none');
-  $('#loader').fadeIn(1000);
-  $('body').addClass('no-scroll');
-});
-
-$(document).ready(function () {
-
   $('#loader-bg').delay(3000).fadeOut(400, function () {
     $('body').removeClass('no-scroll');
   });
   $('#page').slideUp(400).css('display', 'block');
+});
+
+$(document).ready(function () {
+
+  $('#page').css('display', 'none');
+  $('#loader').fadeIn(1000);
+  $('body').addClass('no-scroll');
 
   // set Item bg width
   addItemBgWidth();
